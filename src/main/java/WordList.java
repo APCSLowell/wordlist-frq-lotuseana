a914f033dbb06a7e56ad9535ba8926c9c6675f4d
lotuseana <143060902+lotuseana@users.noreply.github.com>
2024-04-03 09:27:42 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -10,13 +10,23 @@ public class WordList
 
   public int numWordsOfLength(int len)
   {
-    /* to be implemented in part (a) */
-    
+    int count =0;
+    for (int i =0;i<WordList.size();i++){
+      if (WordList.get(i).length()==len){
+        count++;
+      }
+    }
+    return count;
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
+    for (int i =0; i<WordList.size();i++){
+      if (WordList.get(i).length() == len){
+        WordList.remove(i);
+        i--;
+      }
+    }
 
   }
 }
